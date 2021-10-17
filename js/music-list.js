998fb4ec3cc24cd065aef7675347e2cd26ed0cab
Himanshu <helloitshimzz@gmail.com>
2021-10-17 08:41:19 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Save Your Tears",
+    artist: "The Weeknd",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "I Feel It Coming",
+    artist: "The Weeknd",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "In Your Eyes",
+    artist: "The Weeknd",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Talking To The Moon",
+    artist: "Bruno Mars",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Liquor Store Blues",
+    artist: "Bruno Mars",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Treasure",
+    artist: "Bruno Mars",
+    img: "music-6",
+    src: "music-6",
+  },
+];
